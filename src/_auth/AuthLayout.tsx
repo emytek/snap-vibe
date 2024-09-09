a706d58aa110a7841b699cf7e35e1603c9834f02
Nnaemeka Okalla <emyokalla@gmail.com>
2024-09-09 12:03:10 +0100

    auth_signup form appwrite config init v1

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import { Outlet, Navigate } from 'react-router-dom'
+import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
 
 const AuthLayout = () => {
-    const isAuthenticated = false;
+  const isAuthenticated = false;
 
   return (
-    <div>
-        {isAuthenticated ? (
-            <Navigate to='/' />
-        ) : (
-            <>
-               <section>
-                <Outlet />
-              </section> 
-            </>
-        )}
-    </div>
-  )
-}
+    <>
+       {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
 
-export default AuthLayout
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+        </>
+      )}
+    </>
+  );
+};
+
+export default AuthLayout;
