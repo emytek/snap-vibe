0188c28ab78c7afa584fd172cec73bbfcd6c3300
Nnaemeka Okalla <emyokalla@gmail.com>
2024-09-09 07:24:12 +0100

    Setting up vite_typescript_tailwind and shadcn and initializing routes

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import SigninForm from './_auth/forms/SigninForm';
+import './globals.css';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './root/pages';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './root/RootLayout';
+import SignupForm from './_auth/forms/SignupForm';
+
+function App() {
+  return (
+    <>
+       <main className="flex h-screen">
+        <Routes>
+          {/* publc routes */}
+          <Route element={<AuthLayout />}>
+            <Route path="/sign-in" element={<SigninForm />} />
+            <Route path="/sign-up" element={<SignupForm />} />
+          </Route>
+
+          {/* private routes */}
+          <Route element={<RootLayout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+        </Routes>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Outlet, Navigate } from 'react-router-dom'
+
+const AuthLayout = () => {
+    const isAuthenticated = false;
+
+  return (
+    <div>
+        {isAuthenticated ? (
+            <Navigate to='/' />
+        ) : (
+            <>
+               <section>
+                <Outlet />
+              </section> 
+            </>
+        )}
+    </div>
+  )
+}
+
+export default AuthLayout
